412d7169c3bb7ec860be278421917bd499913431
Tony N <tony.ramous@gmail.com>
2015-10-24 13:43:05 -0600

    added monthly visits

--- python/dataeater.py
+++ python/dataeater.py
@@ -21,8 +21,11 @@ def main():
     ParkName = ""
     
     ENTRYMODE_YEAR = 0;
+    ENTRYMODE_MONTHLY = 1;
+    ENTRYMODE_VISITORS = 2;
+    ENTRYMODE_ACTIVITY = 3;
     
-    entryMode = 0;
+    entryMode = 1;
     
     parkData = []
     
@@ -36,6 +39,9 @@ def main():
     
         if(entryMode == ENTRYMODE_YEAR):
             parkData = buildYearlyAttendance(datafile)
+            
+        if(entryMode == ENTRYMODE_MONTHLY):
+            parkData = buildMonthlyAttendance(datafile)
             
             
     parkData["ParkName"] = ParkName
@@ -71,25 +77,66 @@ def buildYearlyAttendance(datafile):
 
         #split the contents into bins
         match = yearGrabber.match(line)
-        print(match)
+        #print(match)
         
         if(match):
             bins = yearGrabber.split(line)
-            year = bins[1]
-            visits = bins[2].replace(",","")
-            annual = bins[3].replace(",","")
             
             if(not(totalVisitsCounted)):
-                totalVisitation = int(annual)
+                totalVisitation = int(bins[3].replace(",",""))
                 totalVisitsCounted = True
                 
-            dataDictionary[year] = int(visits)
+            dataDictionary[bins[1]] = int(bins[2].replace(",",""))
         else:
             break
 
     returnPacket['dataHeader'] = dataHeader
     returnPacket['totalVisitation'] = totalVisitation
     returnPacket['YearlyData'] = dataDictionary
+    
+    return returnPacket
+    
+def buildMonthlyAttendance(datafile):
+
+    monthGrabber = re.compile('(.{4})[,]["](.*)["][,]["](.*)["][,]["](.*)["][,]["](.*)["][,]["](.*)["][,]["](.*)["][,]["](.*)["][,]["](.*)["][,]["](.*)["][,]["](.*)["][,]["](.*)["][,]["](.*)["][,]["](.*)["][\n]')
+    
+    yearIdentifier = re.compile('(.{4})')
+
+    dataHeader = ["Year","1","2","3","4","5","6","7","8","9","10","11","12","AnuualTotal"]
+    
+    dataDictionary = {}
+    returnPacket = {}
+    
+    #skip past the empty lines, the header lines
+    line = datafile.readline()
+    line = datafile.readline()
+
+    
+    while True:
+    
+        #read the line
+        line = datafile.readline()
+
+        #split the contents into bins
+        year = yearIdentifier.match(line)
+        if(year):
+            year = year.group()
+        
+            if(not(year == "2015")):
+                match = monthGrabber.match(line)
+                if(match):
+                    bins = monthGrabber.split(line)
+                    
+                    #print(bins)
+                        
+                    dataDictionary[bins[1]] = [int(bins[2].replace(",","")),int(bins[3].replace(",","")),int(bins[4].replace(",","")),int(bins[5].replace(",","")),int(bins[6].replace(",","")),int(bins[7].replace(",","")),int(bins[8].replace(",","")),int(bins[9].replace(",","")),int(bins[10].replace(",","")),int(bins[11].replace(",","")),int(bins[12].replace(",","")),int(bins[13].replace(",","")),int(bins[14].replace(",",""))]
+                else:
+                    break
+        else:
+            break
+
+    returnPacket['dataHeader'] = dataHeader
+    returnPacket['MonthlyData'] = dataDictionary
     
     return returnPacket
 
