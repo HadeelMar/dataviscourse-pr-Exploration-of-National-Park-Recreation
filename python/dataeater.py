65db47521bc1e9e95914303cacbc654b09868b74
Tony N <tony.ramous@gmail.com>
2015-11-07 19:05:25 -0700

    Finishing third parse type, now writes a JSON file

--- python/dataeater.py
+++ python/dataeater.py
@@ -4,12 +4,16 @@ import re
 
 def main():
 
-    filename = sys.argv[1]
+    filename1 = sys.argv[1] #crunch yearly
+    filename2 = sys.argv[2]
+    filename3 = sys.argv[3]
+    
+    filenames = [filename1,filename2,filename3]
 
-    print ("Processing File: " + filename)
+    
     
     #compare to the second line of the file and it gives you the title
-    titleFindRegex = re.compile('(.*)[,](.*)[\n]')
+    titleFindRegex = re.compile('(.*?)[,](.*)[\n]')
     
     #data file modes are 
     #0 for yearly attendance
@@ -27,30 +31,65 @@ def main():
     
     entryMode = 2;
     
-    parkData = []
+    parkData = {}
     
-    with open (filename, "r") as datafile:
+    for filename in filenames:
+        print ("Processing File: " + filename)
+        with open (filename, "r") as datafile:
         
-        line = datafile.readline()
-        line = datafile.readline()
+            if('yearly' in filename):
+                entryMode = 0
+            elif ('monthly' in filename):
+                entryMode = 1
+            elif ('visitors' in filename):
+                entryMode = 2
+            
+            line = datafile.readline()
+            line = datafile.readline()
+            
+            #get the title of the park
+            ParkName = titleFindRegex.split(line)[1]
+            ParkName = ParkName.replace(" ","_")
         
-        #get the title of the park
-        ParkName = titleFindRegex.split(line)[1]
-    
-        if(entryMode == ENTRYMODE_YEAR):
-            parkData = buildYearlyAttendance(datafile)
+            if(entryMode == ENTRYMODE_YEAR):
+                #returnPacket['DataHeader'] = dataHeader
+                #returnPacket['TotalVisitation'] = totalVisitation
+                #returnPacket['YearlyData'] = dataDictionary
+                datapack = buildYearlyAttendance(datafile)
+                
+                parkData['YearlyDataHeader'] = datapack['YearlyDataHeader']
+                parkData['YearlyTotalVisitation'] = datapack['YearlyTotalVisitation']
+                parkData['YearlyData'] =  datapack['YearlyData']
+                
+            if(entryMode == ENTRYMODE_MONTHLY):
+                datapack = buildMonthlyAttendance(datafile)
+                
+                parkData['MonthlyDataHeader'] = datapack['MonthlyDataHeader']
+                parkData['MonthlyData'] = datapack['MonthlyData']
+                
+            if(entryMode == ENTRYMODE_VISITORS):
             
-        if(entryMode == ENTRYMODE_MONTHLY):
-            parkData = buildMonthlyAttendance(datafile)
-            
-        if(entryMode == ENTRYMODE_VISITORS):
-            parkData = buildMonthlyActivities(datafile)
-            
-            
+                datapack = buildMonthlyActivities(datafile)
+                
+                parkData['ActivityDataHeader'] = datapack['ActivityDataHeader']
+                parkData['ActivityData'] = datapack['ActivityData']
+                
+                
     parkData["ParkName"] = ParkName
+    print(parkData)
+        #if (entryMode == 0):
+        #    verifyYear(parkData)
+        #else if (entryMode == 1):
+        #    verifyMonthly(parkData)
+        #else if(entryMode == 2):
+    verifyPackage(parkData)
     
-    print (parkData)
     
+    
+    outputFile = open(ParkName+".json","w")
+    outputFile.write(json.dumps(parkData))
+    outputFile.close()
+        
     
 def testRegex():
 
@@ -93,8 +132,8 @@ def buildYearlyAttendance(datafile):
         else:
             break
 
-    returnPacket['dataHeader'] = dataHeader
-    returnPacket['totalVisitation'] = totalVisitation
+    returnPacket['YearlyDataHeader'] = dataHeader
+    returnPacket['YearlyTotalVisitation'] = totalVisitation
     returnPacket['YearlyData'] = dataDictionary
     
     return returnPacket
@@ -105,7 +144,7 @@ def buildMonthlyAttendance(datafile):
     
     yearIdentifier = re.compile('(.{4})')
 
-    dataHeader = ["Year","1","2","3","4","5","6","7","8","9","10","11","12","AnuualTotal"]
+    dataHeader = ["1","2","3","4","5","6","7","8","9","10","11","12","AnuualTotal"]
     
     dataDictionary = {}
     returnPacket = {}
@@ -138,14 +177,14 @@ def buildMonthlyAttendance(datafile):
         else:
             break
 
-    returnPacket['dataHeader'] = dataHeader
+    returnPacket['MonthlyDataHeader'] = dataHeader
     returnPacket['MonthlyData'] = dataDictionary
     
     return returnPacket
     
 def buildMonthlyActivities(datafile):
 
-    dataHeader = ["Year","Month","RecreationVisitors","NonRecreationVisitors","ConcessionLodging","TentCampers","RVCampers","ConcessionCamping","BackcountryCampers","MiscCampers","OvernightStays"]
+    dataHeader =            ["RecreationVisitors","NonRecreationVisitors","ConcessionLodging","TentCampers","RVCampers","ConcessionCamping","BackcountryCampers","MiscCampers","OvernightStays"]
     #1979       ,January    ,"2,970",0,0        ,35         ,47 ,0      ,5      ,0  ,87
     
     dataDictionary = {}
@@ -169,7 +208,6 @@ def buildMonthlyActivities(datafile):
     
         #read the line
         line = datafile.readline()
-        print(line)
         
         if(len(line) > 0):
         
@@ -194,10 +232,10 @@ def buildMonthlyActivities(datafile):
                     cleanedInput += line
                     line = ""
         
+            #once the file read is done
             else:
-                #break;
-            
-            
+                aggregateAnotherMonth = False
+                
                 resultBins = cleanedInput.split(',')
                 
                 if(len(resultBins) > 1):
@@ -205,19 +243,22 @@ def buildMonthlyActivities(datafile):
                     for i in range(0,len(resultBins)):
                         if(resultBins[i] == ""):
                             resultBins[i] == 0
-                            
-                    previousYear = currentYear
+                                   
+                    previousYear = currentYear        
                     currentYear = resultBins[0]
+                    
+                    #if we changed years, clear the activity package
+                    if(not(previousYear == currentYear)): 
+                        activityPackage = {}
 
                     currentMonth = resultBins[1]
-                    print(currentMonth)
 
                     massagedVisitorCounts = resultBins[2:]
                     massagedVisitorCounts = massagedVisitorCounts[:9]
+                    
+                    activityPackage[lookupMonthByName(currentMonth)] = massagedVisitorCounts
 
-                    print(resultBins)
-                    print(massagedVisitorCounts)
-
+                    dataDictionary[currentYear] = activityPackage
                 
                     cleanedInput = ""
                 
@@ -226,7 +267,72 @@ def buildMonthlyActivities(datafile):
             
 
 
+    returnPacket['ActivityDataHeader'] = dataHeader
+    returnPacket['ActivityData'] = dataDictionary
+    return returnPacket
     #index 2 - 10
+    
+def verifyPackage(dataPackage):
+    print("Verification of data package has started")
+    print("----------------------------------------")
+    print(dataPackage["ParkName"])
+    verifyActivity(dataPackage)
+    verifyMonthly(dataPackage)
+    
+    
+def verifyActivity(dataPackage):
+    print("----------------------------------------")
+    print("ActivityData")
+    print("----------------------------------------")
+    for element in dataPackage["ActivityDataHeader"]:
+        print ("[" + element + "]")
+    print("----------------------------------------")
+    print("-ActivityData")
+    for year in dataPackage['ActivityData']:
+        print("---["+year+"]")
+        for month in dataPackage['ActivityData'][year]:
+            print("------["+str(month)+"]")
+            prepstring = ("|".join(str(x) for x in dataPackage['ActivityData'][year][month]))
+            
+            print ("--------"+prepstring)
+            
+def verifyMonthly(dataPackage):
+    print("----------------------------------------")
+    print("MonthlyData")
+    print("----------------------------------------")
+    for element in dataPackage["MonthlyDataHeader"]:
+        print ("[" + element + "]")
+    print("----------------------------------------")
+    for element in dataPackage["MonthlyData"]:
+        print ("[" + element + "]")
+        prepstring = ("|".join(str(x) for x in dataPackage['MonthlyData'][element]))
+        print ("--------"+prepstring)
+    
+def lookupMonthByName(name):
+    if(name.lower() == "january"):
+        return 1
+    if(name.lower() == "february"):
+        return 2
+    if(name.lower() == "march"):
+        return 3
+    if(name.lower() == "april"):
+        return 4
+    if(name.lower() == "may"):
+        return 5
+    if(name.lower() == "june"):
+        return 6
+    if(name.lower() == "july"):
+        return 7
+    if(name.lower() == "august"):
+        return 8
+    if(name.lower() == "september"):
+        return 9
+    if(name.lower() == "october"):
+        return 10
+    if(name.lower() == "november"):
+        return 11
+    if(name.lower() == "december"):
+        return 12
     
             
 if __name__ == "__main__":
